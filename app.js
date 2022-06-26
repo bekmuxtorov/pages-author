94ac67a63c62f8074fe3b5ced9fcf4a9c12d6df1
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-06-27 00:46:45 +0500

    header section desktop device coplated

---
+++ app.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector(".menu-icon"),
+    navMenu = document.querySelector(".nav-menu");
+
+function menuList() {
+    if (navMenu.style.tra){
+        navMenu.scale(1,)
+    }
+}
